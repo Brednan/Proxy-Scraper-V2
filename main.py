d22415b1638fe4c284ebe8eebbb9915df591a085
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-04-09 14:04:05 -0700

    Working on GUI

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from GUI_Components import *
 
 window = MainWindow(size=(600, 600), is_resizeable=False, icon='./images/app_icon.ico', title='Proxy Scraper V2')
 window.window.mainloop()
+print('hi')
