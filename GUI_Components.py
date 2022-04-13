bff742fe99fa7d974343944074bf475620fa26e8
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-04-13 13:48:43 -0700

    Working on GUI Components

--- GUI_Components.py
+++ GUI_Components.py
@@ -2,11 +2,26 @@ import tkinter as tk
 
 
 class MainWindow:
-    def __init__(self, size: tuple, is_resizeable: bool, icon, title):
+    def __init__(self, size: tuple, is_resizeable: bool, icon, title, bg:str):
         self.window = tk.Tk()
         self.window.geometry(f'{size[0]}x{size[1]}')
         self.window.resizable(width=is_resizeable, height=is_resizeable)
         self.window.iconbitmap(icon)
         self.window.title(title)
+        self.window.config(bg=bg)
 
+class Title:
+    def __init__(self, master, text:str, color:str, font:tuple, pos:tuple, bg:str):
+        self.title = tk.Label(master, text=text, fg=color, font=font, bg=bg)
+        self.title.place(x=pos[0], y=pos[1], anchor=tk.CENTER)
 
+class OutputFolder:
+    def __init__(self, master, width:int, font:tuple, pos:tuple, bg:str):
+        self.entry = tk.Entry(master, width=width, font=font)
+        self.entry.place(x=pos[0], y=pos[1], anchor=tk.SW)
+
+        self.button = tk.Button(master, font=font, text='...')
+        self.button.place(anchor=tk.SW, x=pos[0] + 340, y=pos[1], width=70, height=30)
+
+        self.label = tk.Label(master, font=font, text='Output Dir:', fg='white', bg=bg)
+        self.label.place(anchor=tk.SE, x=pos[0] - 10, y=pos[1])
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from GUI_Components import *
 
-window = MainWindow(size=(600, 600), is_resizeable=False, icon='./images/app_icon.ico', title='Proxy Scraper V2')
+background_color = '#343434'
+
+window = MainWindow(size=(600, 700), is_resizeable=False, icon='./images/app_icon.ico', title='Proxy Scraper V2', bg=background_color)
+
+title = Title(window.window, 'Proxy Scraper V2', 'white', font=('default', 30), pos=(300, 50), bg=background_color)
+
+output_dir = OutputFolder(window.window, 25, ('default', 17), (150, 150), bg=background_color)
+
 window.window.mainloop()
