699af6da40687fbffc48550791c769379ca8b35f
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-05-04 18:23:16 -0700

    Made the first site function iterate through a list of urls for that specific site. Then it will add the proxies to a list, and filter out the duplicates

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ class Scraper:
     def __init__(self, types):
         self.https = False
         self.socks4 = False
+        self.proxies_scraped = []
 
         if types.http_checked.get() == 1:
             self.https = True
@@ -14,28 +15,43 @@ class Scraper:
             self.socks4 = True
 
     def scrape_proxies(self):
-        site1 = Site1('https://free-proxy-list.net/')
+        site1 = Site1(self)
         site1.get_proxies()
+        print(self.proxies_scraped)
 
 
 class Site1:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, main_class):
+        self.urls = ('https://free-proxy-list.net/', 'https://www.sslproxies.org/')
+        self.main = main_class
 
-    def get_site_content(self):
-        page = req.get(self.url)
+    def get_site_content(self, url):
+        page = req.get(url)
         soup = BeautifulSoup(page.text, 'html.parser')
         return soup
 
     def get_proxies(self):
-        soup = self.get_site_content()
+        proxies = []
+        for url in self.urls:
+            soup = self.get_site_content(url)
 
-        proxy_list = soup.find(id='list').div.find('tbody')
+            proxy_list = soup.find(id='list').div.find('tbody')
 
-        for proxy_item in proxy_list:
-            proxy_info = proxy_item.find_all('td')
+            for proxy_item in proxy_list:
+                proxy_info = proxy_item.find_all('td')
 
-            ip = proxy_info[0].get_text()
-            port = proxy_info[1].get_text()
+                ip = proxy_info[0].get_text()
+                port = proxy_info[1].get_text()
 
-            print(f'{ip}:{port}')
+                proxies.append(f'{ip}:{port}')
+
+        proxies = list(dict.fromkeys(proxies))
+
+        for proxy in proxies:
+            proxy = proxy.split(':')
+
+            self.main.proxies_scraped.append({
+                'ip': proxy[0],
+                'port': proxy[1],
+                'type': 'http'
+            })
