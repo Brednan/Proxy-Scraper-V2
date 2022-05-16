c8cda1a8e33046867ea25a697655d3ceecdb8f75
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-05-16 13:58:04 -0700

    Used selenium to scrape the proxies on the second site, because when I tried to use the requests module to get the content, it identified me as a bot.

--- save_proxies.py
+++ save_proxies.py
@@ -2,20 +2,10 @@ class SaveProxies:
     def __init__(self, output):
         self.output = output
 
-    def save_http(self, proxies):
-        output_file = open(self.output, 'w')
-        output_file.write('Http Proxies:\n')
-
-        for p in proxies:
-            output_file.write(f'p\n')
-
-        output_file.close()
-
-    def save_socks(self, proxies):
+    def save_proxies(self, proxies):
         output_file = open(self.output, 'a')
-        output_file.write('Socks4 Proxies:\n')
 
         for p in proxies:
-            output_file.write(f'p\n')
+            output_file.write(f'{p}\n')
 
         output_file.close()
--- scraper.py
+++ scraper.py
@@ -1,47 +1,22 @@
+import time
+
 import requests as req
 from bs4 import BeautifulSoup
-import threading
-from checker import Checker
-
-class Scraper:
-    def __init__(self, types):
-        self.https = False
-        self.socks4 = False
-        self.http_proxies_scraped = []
-        self.socks4_proxies_scraped = []
-
-        if types.http_checked.get() == 1:
-            self.https = True
-
-        if types.socks4_checked.get() == 1:
-            self.socks4 = True
-
-    def scrape_proxies(self):
-        site1 = Site1(self)
-
-        if self.https == 1:
-            site1.get_http_proxies()
-
-        if self.socks4 == 1:
-            site1.get_socks_proxies()
-
+from save_proxies import SaveProxies
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Site1:
     def __init__(self, main_class):
         self.http_proxy_urls = ('https://free-proxy-list.net/', 'https://www.sslproxies.org/', 'https://www.us-proxy.org/')
-        self.socks4_url = 'https://www.socks-proxy.net/'
         self.main = main_class
 
-    def get_site_content(self, url):
-        page = req.get(url)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        return soup
-
-    def get_http_proxies(self):
+    def get_http_proxies_site_1(self):
         http_proxies = []
         for url in self.http_proxy_urls:
-            soup = self.get_site_content(url)
+            soup = self.main.get_site_content(url)
 
             proxy_list = soup.find(id='list').div.find('tbody')
 
@@ -58,21 +33,41 @@ class Site1:
         for proxy in http_proxies:
             self.main.http_proxies_scraped.append(proxy)
 
-    def get_socks_proxies(self):
-        socks4_proxies = []
-        soup = self.get_site_content(self.socks4_url)
 
-        proxy_list = soup.find(id='list').div.find('tbody')
+class Site2(Firefox, By):
+    def __init__(self, main_class):
+        self.main = main_class
+        self.proxy_lists = ('https://openproxy.space/list/http',)
+        Firefox.__init__(self)
 
-        for proxy_item in proxy_list:
-            proxy_info = proxy_item.find_all('td')
+    def get_site_2_http_proxies(self):
+        self.get(self.proxy_lists[0])
+        time.sleep(2)
 
-            ip = proxy_info[0].get_text()
-            port = proxy_info[1].get_text()
+        textbox = self.find_element(self.XPATH, '/html/body/div[1]/div/div/div/div[1]/section[4]/textarea')
+        proxy_list = textbox.text.rstrip().split('\n')
 
-            socks4_proxies.append(f'{ip}:{port}')
+        for proxy in proxy_list:
+            self.main.http_proxies_scraped.append(proxy)
 
-        socks4_proxies = list(dict.fromkeys(socks4_proxies))
+        self.quit()
 
-        for proxy in socks4_proxies:
-            self.main.socks4_proxies_scraped.append(proxy)
+
+class Scraper(Site1, Site2, SaveProxies):
+    def __init__(self, output_path):
+        SaveProxies.__init__(self, output_path)
+        Site1.__init__(self, self)
+        Site2.__init__(self, self)
+
+        self.http_proxies_scraped = []
+        self.output_path = output_path
+
+    def scrape_proxies(self):
+        self.get_http_proxies_site_1()
+        self.get_site_2_http_proxies()
+        self.save_proxies(self.http_proxies_scraped)
+
+    def get_site_content(self, url):
+        page = req.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        return soup
